c49373a02cfd249ea9a8ae59e7ac4e9f32c55eda
hakimshokri <hakim18196@gmail.com>
2025-02-16 00:48:38 +0800

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://bonda-cottage-backend.onrender.com"
 
     const [cottages, setCottages] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
